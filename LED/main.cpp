24b0c37ae08c6b2b64bb977c7e1d68e7ca686674
heejungPark <hjpark@sigma-delta.tech>
2020-10-19 15:37:05 +0900

    Add LED example

---
+++ LED/main.cpp
@@ -0,0 +1,24 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE       1000
+#define PORT_LED            PA_11
+
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+    DigitalOut led(PORT_LED);
+
+    while (true) {
+        led = !led;
+        printf("Toggle\n");
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
