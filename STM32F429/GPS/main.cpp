b5eb2d589d6a75d27ec43911145bd6e2a4a5b16c
heejungPark <hjpark@sigma-delta.tech>
2020-11-13 11:30:33 +0900

    Update GPS

--- STM32F429/GPS/main.cpp
+++ STM32F429/GPS/main.cpp
@@ -5,20 +5,61 @@
 
 #include "mbed.h"
 
-
 // Blinking rate in milliseconds
-#define BLINKING_RATE       1000
-#define PORT_LED            PA_11
+#define BLINKING_RATE 1000
 
+Serial gps(MBED_CONF_APP_GPS_UART_TX, MBED_CONF_APP_GPS_UART_RX, 9600);
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut led(PORT_LED);
+    char GPS_c;
+    char GPS_buffer[512];
+    int GPS_i = 0;
+    char address_buffer[8];
+    int address_i = 0;
+    bool address_reading = false;
 
-    while (true) {
-        led = !led;
-        printf("Toggle\n");
-        ThisThread::sleep_for(BLINKING_RATE);
+    printf("\nGPS MESSAGE:\n");
+
+    while (true)
+    {
+        if (gps.readable())
+        {
+            GPS_c = gps.getc();
+
+            if (GPS_c == '$' && GPS_i > 0)
+            {
+                // Check if it is the first gps address (GNGLL)
+                if (address_buffer[0] == 'G' && address_buffer[1] == 'N' && address_buffer[2] == 'G' && address_buffer[3] == 'L' && address_buffer[4] == 'L')
+                {
+                    printf("\nGPS MESSAGE:\n");
+                }
+
+                // Print out a single line of gps message
+                for (int j = 0; j < GPS_i; j++)
+                {
+                    printf("%c", (char)GPS_buffer[j]);
+                }
+
+                GPS_buffer[0] = GPS_c;
+                GPS_i = 1;
+
+                address_i = 0;
+                address_reading = true;
+            }
+            else
+            {
+                GPS_buffer[GPS_i++] = GPS_c;
+
+                if (address_reading && GPS_c == ',')
+                {
+                    address_reading = false;
+                }
+                else if (address_reading)
+                {
+                    address_buffer[address_i++] = GPS_c;
+                }
+            }
+        }
     }
 }
